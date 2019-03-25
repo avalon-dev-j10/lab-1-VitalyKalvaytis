9074505599c342bad34921f32cb3f18ad48fddd0
VitalyKalvaytis <35275702+VitalyKalvaytis@users.noreply.github.com>
2019-03-26 01:12:38 +0300

    Update Person.java

--- src/ru/avalon/java/dev/j10/labs/models/Person.java
+++ src/ru/avalon/java/dev/j10/labs/models/Person.java
@@ -11,7 +11,71 @@ package ru.avalon.java.dev.j10.labs.models;
  *     <li>пропиской по месту жительства.
  * </ol>
  */
+import ru.avalon.java.dev.j10.labs.commons.Address;
+
 public class Person {
+    private String name;
+    private Passport thisPassport;
+    private Address thisAddress;
+    
+    public Person(String name, Passport thisPassport, Address thisAddress){
+        this.name = getFullName();
+        this.thisPassport = thisPassport;
+        this.thisAddress = thisAddress;
+    }
+
+    public Person(Passport thisPassport, Address thisAddress) {
+        this.thisPassport = thisPassport;
+        this.thisAddress = thisAddress;
+    }
+    
+    public String getFullName() {
+        /*
+         * TODO(Студент): Закончить определение метода 'getFullName()' класса 'Person'
+         */
+        String pName = thisPassport.getName();
+        String surname = thisPassport.getSurname();
+        String firstName = thisPassport.getFirstname();
+        String secondName = thisPassport.getSecondname();
+
+        if (pName != null && surname != null && firstName != null){
+            return surname + " " + pName + " " + firstName;
+        }
+
+        if (firstName == null && secondName != null) {
+            return pName + " " + secondName.substring(0 ,1).toUpperCase() + ". " + surname;
+        }
+
+        if (firstName == null && secondName == null) {
+            return pName + " " + surname;
+        }
+        return "";
+    }
+    
+    public String getAddress() {
+        /*
+         * TODO(Студент): Закончить определение метода 'getAddress()' класса 'Person'
+         */
+        String myAdd = thisAddress.getFullAddress();
+        
+        if (!(myAdd.length() == 0)){ 
+            return myAdd;}
+        return "";
+    }
+    
+    public void setThisPassport(Passport thisPassport) {
+        this.thisPassport = thisPassport;
+    }
+
+    public void setThisAddress(Address thisAddress) {
+        this.thisAddress = thisAddress;
+    }
+    
+    public Passport getThisPassport() {
+        return thisPassport;
+    }
+
+}
 
     /*
      * TODO(Студент): Создайте класс Address.
@@ -46,12 +110,7 @@ public class Person {
      *
      * @return имя человека в виде строки.
      */
-    public String getFullName() {
-        /*
-         * TODO(Студент): Закончить определение метода 'getFullName()' класса 'Person'
-         */
-        return null;
-    }
+
 
     /**
      * Возвращает адрес, по которому проживает человек.
@@ -61,10 +120,4 @@ public class Person {
      *
      * @return адрес регистрации в виде строки.
      */
-    public String getAddress() {
-        /*
-         * TODO(Студент): Закончить определение метода 'getAddress()' класса 'Person'
-         */
-        return null;
-    }
-}
+
