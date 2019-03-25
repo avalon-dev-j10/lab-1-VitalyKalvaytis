25b6f960cfc1354b019986389cb3b85176e620c1
VitalyKalvaytis <35275702+VitalyKalvaytis@users.noreply.github.com>
2019-03-26 01:14:05 +0300

    Update Address.java

--- src/ru/avalon/java/dev/j10/labs/commons/Address.java
+++ src/ru/avalon/java/dev/j10/labs/commons/Address.java
@@ -1,3 +1,4 @@
+
 /*
  * TODO(Студент): Создайте класс Address.
  *
@@ -13,3 +14,45 @@
  * 4. Подумайте над тем, какие методы должны быть объявлены
  *    в классе.
  */
+package ru.avalon.java.dev.j10.labs.commons;
+
+public class Address{
+    private String country;
+    private String city;
+    private String street;
+    private String house;
+    private String apartment;
+
+
+    public Address(String country, String city, String street, String house, String apartment) {
+        this.country = country;
+        this.city = city;
+        this.street = street;
+        this.house = house;
+        this.apartment = apartment;
+    }
+       
+    public String getCountry(){
+        return country;
+    }
+    
+    public String getCity(){
+        return city;
+    }
+    
+    public String getStreet(){
+        return street;
+    }
+
+    public String getHouse(){
+        return house;
+    }
+    
+    public String getApartment(){
+        return apartment;
+    }
+    
+    public String getFullAddress(){
+        return "Страна: " + country + " Город: " + city + " Улица: " + street + " Дом: " + house + " Квартира: " + apartment;
+    } 
+}
